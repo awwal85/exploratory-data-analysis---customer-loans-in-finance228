231424359b04e2ad7867dcea5dc7882bca1e9381
Mohammed Awwal Alidu <awwal85@yahoo.com>
2023-12-25 05:34:04 +0000

    Created db_utils.py file

---
+++ db_utils.py
@@ -0,0 +1,24 @@
+import sqlalchemy as db
+from sqlalchemy import create_engine
+import pandas as pd
+import yaml
+
+class RDSDatabaseConnector:
+    with open('credentials.yaml') as f:
+        credentials_file = yaml.safe_load(f)
+        
+    def __init__(self,credentials_file):
+        self.credentials.yaml = credentials_file
+    
+# Extracting data from the RDS database as a Pandas DataFrame
+engine = create_engine('postgresql://loansanalyst:EDAloananalyst@eda-projects.cq2e8zno855e.eu-west-1.rds.amazonaws.com:5432/payments')
+conn = engine.connect()
+table_df = pd.read_sql_table("loan_payments", con = engine) 
+print(table_df)
+
+# Saves the data as a csv file in my local machine
+table_df.to_csv(r'C:\Users\Cex- pc\oneDrive\Desktop\Data Analysis\loan_payments.csv')
+
+#  Loading the data from my local machine into a Pandas DataFrame.
+df = pd.read_csv('loan_payments.csv')
+df.head()
