a9564da20ed51e7133af1fe8f225fcb63ba95148
Mohammed Awwal Alidu <awwal85@yahoo.com>
2024-01-02 03:41:09 +0000

    remove DataTransform.py

--- DataTransform.py
+++
@@ -1,9 +0,0 @@
-import datetime as datetime
-import pandas as pd
-# This class handles all the column conversions to the appropriate format
-class DataTransform:
-    loans_df = pd.read_csv('loan_payments.csv')
-    del loans_df['Unnamed: 0']
-    df = loans_df.astype({'issue_date':'datetime64','earliest_credit_line':'datetime64',
-                          'last_payment_date':'datetime64', 'next_payment_date': 'datetime64', 
-                          'last_credit_pull_date':'datetime64'})
